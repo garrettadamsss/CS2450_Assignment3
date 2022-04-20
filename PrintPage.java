decfd059df22f5cba38a264f5ca1ab36b11d44de
alfredzhuang <alfredzhuang123@gmail.com>
2022-04-19 22:35:06 -0700

    add basic options (destionation, pages, copies, and layout)

--- PrintPage.java
+++ PrintPage.java
@@ -1,6 +1,14 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+
 /**
  * Print Page
  */
@@ -14,7 +22,44 @@ public class PrintPage extends Application
 
   @Override
   public void start(Stage primaryStage)
-  {
+  { 
+    // Destination option
+    Label destinationLabel = new Label("Destination");
+    ComboBox<String> destinationComboBox = new ComboBox<>();
+    destinationComboBox.getItems().addAll("Printer 1", "Printer 2", "Save as PDF");
+    destinationComboBox.setValue("Printer 1");
+    HBox destinationHBox = new HBox(15, destinationLabel, destinationComboBox);
+
+    // Pages option
+    Label pagesLabel = new Label("Pages");
+    ComboBox<String> pagesComboBox = new ComboBox<>();
+    pagesComboBox.getItems().addAll("All", "Odd pages Only", "Even pages Only", "Custom");
+    pagesComboBox.setValue("All");
+    HBox pagesHBox = new HBox(15, pagesLabel, pagesComboBox);
+
+    // Copies option
+    Label copiesLabel = new Label("Copies");
+    Spinner<Integer> copiesSpinner = new Spinner<>(1, 100, 1);
+    HBox copiesHBox = new HBox(15, copiesLabel, copiesSpinner);
+
+    // Layout option
+    Label layoutLabel = new Label("Layout");
+    ComboBox<String> layoutComboBox = new ComboBox<>();
+    layoutComboBox.getItems().addAll("Portrait", "Landscape");
+    layoutComboBox.setValue("Portrait");
+    HBox layoutHBox = new HBox(15, layoutLabel, layoutComboBox);
+
+    // Vertically align all of the options
+    VBox optionsVBox = new VBox(10, destinationHBox, pagesHBox, copiesHBox, layoutHBox);
+
+    // Create a scene
+    Scene printPageScene = new Scene(optionsVBox);
+
+
+
+    // Set the scene of the stage
+    primaryStage.setScene(printPageScene);
+
     // Set the title of the window
     primaryStage.setTitle("Print Page");
 
